b0ed8c3264b46f3589610a79d451f5841e47d25b
Urvashi Verma <131573907+UrvashiV@users.noreply.github.com>
2026-07-01 23:43:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Hands of Straights/hands-of-straights.java
@@ -0,0 +1,32 @@
+class Solution {
+    static boolean isStraightHand(int n, int groupSize, int hand[]) {
+        // code here
+      
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+
+        if(n % groupSize != 0) return false;
+
+        for(int i : hand){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+
+        while(!map.isEmpty()){
+            int first = map.firstKey();
+
+            for(int i=0 ;i<groupSize;i++){
+                int curr = first +i;
+                if(!map.containsKey(curr)) return false;
+
+                map.put(curr,map.get(curr)-1);
+
+                if(map.get(curr)==0){
+                    map.remove(curr);
+                }
+            }
+
+        }
+
+        return true;
+
+    }
+}
