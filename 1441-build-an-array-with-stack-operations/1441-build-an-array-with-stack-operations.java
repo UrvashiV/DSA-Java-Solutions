e944f75e178e1d69c0696f9683711dc47bb6c31c
Urvashi Verma <131573907+UrvashiV@users.noreply.github.com>
2026-08-11 23:44:12 +0530

    Time: 1 ms (14.47%), Space: 44.5 MB (15.89%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -2,16 +2,19 @@ class Solution {
     public List<String> buildArray(int[] target, int n) {
 
         List<String> ans = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
         int stream =1;
         int i=0;
 
         while(i<target.length && stream <=n){
+            stack.push(stream);
             ans.add("Push");
 
             if(stream == target[i]){
                 i++;
             }
             else{
+                stack.pop();
                 ans.add("Pop");
             }
 
